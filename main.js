106b9a0ef86381c9ae8dd29b9bc5c1e7dcc336b2
mateopeduto <mateoplegnazzi@gmail.com>
2025-07-27 17:37:46 -0300

    Actualizo el link de apps script

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.getElementById("videoForm").addEventListener("submit", async function (
       if (formData.has("file") || formData.has("link")) {
         status.textContent = "Subiendo...";
         try {
-          const response = await fetch("URL_DEL_GOOGLE_APPS_SCRIPT", {
+          const response = await fetch("https://script.google.com/macros/s/AKfycbxnkCldyiUVjQGQC7DLE4aeZyO5fdfaKJlW_zzwiv4MlG1QOrC_MBuv6Avfy_oWH1__Lg/exec", {
             method: "POST",
             body: formData
           });
